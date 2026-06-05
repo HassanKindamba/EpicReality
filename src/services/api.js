30d33ac867865baa18947a90dc18e540e30f32d4
HassanKindamba <kindambahassani2005@gmail.com>
2026-06-05 19:40:13 +0300

    Update frontend API service

--- src/services/api.js
+++ src/services/api.js
@@ -233,6 +233,29 @@ export const getMessages = async (userId) => {
   return response.data;
 };
 
+
+  import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://YOUR-LARAVEL-DOMAIN/api",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
+
 /*
 |--------------------------------------------------------------------------
 | DASHBOARD
