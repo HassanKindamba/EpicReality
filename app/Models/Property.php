e2c076675988644e50859d6f042da78e8aa4dd03
HassanKindamba <kindambahassani2005@gmail.com>
2026-06-09 15:28:13 +0300

    Update Property model

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -18,22 +18,19 @@ class Property extends Model
         'visibility_status',
         'price',
         'image',
-        'user_id', // Add user_id to fillable
+        'user_id',
     ];
 
-    
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-     // Hii ndio method ya bedrooms
     public function bedrooms()
     {
         return $this->hasMany(Bedroom::class);
     }
 
-    //Hii ndio method ya bathroom
     public function bathrooms()
     {
         return $this->hasMany(Bathroom::class);
@@ -42,10 +39,5 @@ class Property extends Model
     public function images()
     {
         return $this->hasMany(PropertyImage::class);
-    }
-
-    public function properties()
-    {
-        return $this->hasMany(Property::class);
     }
 }
